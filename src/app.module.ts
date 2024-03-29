07211ff84077f0a80eea5ea78a60a9728c532b4c
J-hoplin1 <jhoplin7259@gmail.com>
2024-03-30 01:31:31 +0900

    Feat: Initialize typeorm root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from './logger/logger.module';
 import { LoggerMiddleware } from './logger/logger.middleware';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [LoggerModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'postgres',
+          password: 'hoplin1234!',
+          database: 'mogakoffee',
+          entities: [`${__dirname}/**/*.entity.{.ts,.js}`],
+          synchronize: config.get<string>('MODE') === 'development', // Set synchronize in development mode
+        };
+      },
+    }),
+    LoggerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
